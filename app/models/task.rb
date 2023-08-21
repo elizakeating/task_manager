cac15424679ea11ee732a4e24d5349d75c13ab07
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-21 15:50:32 -0600

    feat: add laundry? method to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
-  
+  def laundry?
+    if title.downcase.include?("laundry") or description.downcase.include?("laundry")
+      true
+    else
+      false
+    end
+  end
 end
